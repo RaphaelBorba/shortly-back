d97b65f15fcf82c742b4ff271aba8eb56a747c6e
Raphael Borba <raphaelbsx@gmail.com>
2022-12-19 17:34:50 -0300

    feat: postUrlShort done

--- dump.sql
+++ dump.sql
@@ -16,3 +16,12 @@ CREATE TABLE sessions(
     user_id INTEGER NOT NULL REFERENCES users(id),
     created_at DATE NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE urls(
+    id SERIAL PRIMARY KEY,
+    short_url CHAR(20) NOT NULL,
+    url TEXT NOT NULL,
+    visit_count INTEGER NOT NULL DEFAULT 0,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    created_at DATE NOT NULL DEFAULT NOW()
+);
--- src/Controllers/urls.controller.js
+++ src/Controllers/urls.controller.js
@@ -1,11 +1,38 @@
 import { connection } from "../Database/db.js"
+import { urlModel } from "../Models/urls.model.js"
+import { nanoid } from "nanoid"
 
 
 
 export async function postUrlShort(req ,res){
 
+    const body = req.body
     const {userId} = res.locals
 
+    const validation = urlModel.validate(body)
 
-    res.status(200).send(res.locals)
+    if(validation.error){
+        return res.sendStatus(422)
+    }
+
+    const alreadyCreate = await connection.query(`SELECT * FROM urls WHERE url=$1 AND user_id=$2;`, [body.url,userId])
+
+    if(alreadyCreate.rows[0]){
+        return res.sendStatus(409)
+    }
+
+    const shortUrl = nanoid(6)
+
+    try {
+        
+        await connection.query(`INSERT INTO urls(short_url, url, user_id) VALUES ($1,$2,$3);`, [shortUrl, body.url, userId])
+
+        res.status(200).send(shortUrl)
+
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+
+    
 }
